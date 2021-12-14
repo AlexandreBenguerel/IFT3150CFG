d81e30e7f2ada6cfa3a00f70a84c577308059d7e
AlexandreBenguerel <80494349+AlexandreBenguerel@users.noreply.github.com>
2021-12-14 13:15:26 -0500

    Dernier Commit avant la présentation

--- CFG.lean
+++ CFG.lean
@@ -1,6 +1,8 @@
 /-
 Grammaire hors context
 Auteur: Alexandre Benguerel
+
+Version pré-présentation: 14 décembre 2021
 -/
 
 import computability.language
@@ -26,12 +28,19 @@ namespace CFG
 
 variables {a : Type*} {v : Type*} (G : CFG a v)
 
+/-
+Permet de convertir d'une liste de terminaux vers une liste de caractères
+-/
 instance charac.has_coe_a : has_coe a (charac a v) := ⟨@charac.term a v⟩
 instance : has_coe (list a) (list (charac a v)) := ⟨list.map coe⟩
 
 instance [inhabited v] : inhabited (CFG a v) :=
 ⟨CFG.mk (default v) ∅⟩
 
+/-
+Définition de base du fonctionnement de la grammaire
+Problème avec la récurtion. Possible problème avec comment j'essaie de définition cela.
+-/
 def eval_from : v → list (charac a v) → Prop
 | start w :=
 ∃ r ∈ G.Rules, ∃ a b c, w = a ++ b ++ c ∧ b ≠ [] ∧ by exact r start = b 
@@ -40,17 +49,15 @@ def eval_from : v → list (charac a v) → Prop
 def eval (w : list (charac a v)) : Prop :=
 G.eval_from G.Start w
 
-/-
-Probleme, conversion entre `list a` et `list charac`?
--/
 def accepts : language a :=
 λ x, G.eval x
 
-/-
-Encore la meme chose, conversion entre `list a` et `list charac`?
--/
 lemma mem_accepts (m : list a) : m ∈ G.accepts ↔ G.eval_from G.Start m := by refl
 
+/-
+Lemme principal de la preuve
+Énoncé seulement, il reste d'autres choses à corriger avant de passer à cette preuve.
+-/
 lemma pumping_lemma :
   ∃ p ≥ 1, ∀ x : list a, x ∈ G.accepts ∧ x.length ≥ p → 
   ∃ a b c d e, x = a ++ b ++ c ++ d ++ e ∧ b ++ d ≠ [] ∧ b.length + c.length + d.length ≤ p ∧ 
